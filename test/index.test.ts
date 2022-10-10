68534303cb0add1feedbf71033f683ea98c794c5
Weijian Yang <weijian.yang@rea-group.com>
2022-10-10 17:02:00 +0800

    Fix: eslint config.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
-import * as assert from "assert";
+import * as assert from 'assert';
 
 describe('hello world', () => {
     it('whatever', () => {
-        assert.equal(1,1);
+        assert.equal(1, 1);
     });
-})
+});
